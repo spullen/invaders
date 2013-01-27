bb4190300f69a5b0635e44cc065348534374bf9c
Scott Pullen <s.pullen05@gmail.com>
2013-01-27 16:19:41 -0500

    fixing some spacing for the Game class

--- src/spullen/com/invaders/Game.java
+++ src/spullen/com/invaders/Game.java
@@ -61,7 +61,7 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public synchronized void stop() {
-	    running = false;
+		running = false;
 	    try {
 	        thread.join();
 	    } catch (InterruptedException e) {
@@ -150,14 +150,14 @@ public class Game extends Canvas implements Runnable {
 	
 	public static void main(String[] args) {
 		Game game = new Game();
-	    game.frame.setResizable(false);
+		game.frame.setResizable(false);
 		game.frame.setTitle(Game.title);
 		game.frame.add(game);
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-	    
-	    game.start();
+		
+		game.start();
 	}
 }
